3cb978a8daf55ce1bc19607b277dbecc56424ccc
kadr2022 <kadr@wavelet.net>
2022-12-03 17:13:15 +0800

    factorial

---
+++ Day-11/js/loop-ex-6-factorial.js
@@ -0,0 +1,25 @@
+// **************************************  Factorial ***************************************************************
+/*
+Заавар: n! - n факториал гэж уншина. Энэ нь 1,2,3,…,n тоонуудын үржвэр байна
+n!=1⋅2⋅3⋅⋯⋅n
+
+5! = 120 / 1*2*3*4*5/
+
+Бодлого : 
+
+prompt - оор гараас тоо авч тэр тооныхоо факториалыг олох.
+*/
+
+const maxNumber = Number(prompt("Factorial exercise\nEnter number"));
+let i = 1;
+let sum = 1;
+//let text = "";
+while (i <= maxNumber) {
+    sum = sum * i;
+    //text = text + "*" + i;
+    i++;
+    console.log("sum = " + sum);
+}
+
+//console.log(text);
+console.log(maxNumber + "'s factorial is = " + sum);
