f7bdf2bd1b5914c19519b3f80e3a7375cae22c44
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-09 20:20:37 +0800

    js object exercise

--- z-Assignments/js-object/object-5-MERN-object/main.js
+++ z-Assignments/js-object/object-5-MERN-object/main.js
@@ -71,16 +71,59 @@ const users = [
 console.table(JSON.stringify(users[1], null, 4));
 
 // 2. Хэрэглэгчийн объектод олон ур чадвар эзэмшсэн хүнийг олоорой.
-function findSkilled (users) {
-    let temp = [];
-    for(let i = 0; i < users.length; i++) {
-        console.log("Test");
-        console.log(users[i].skills.length);
+console.log('\n2. Хэрэглэгчийн объектод олон ур чадвар эзэмшсэн хүнийг олоорой.');
+findSkilled(users)
+
+function findSkilled (array) {
+    let skillsNumber = array[0];
+    for(let i = 0; i < array.length; i++) {
+      if(Number(skillsNumber.skills.length) < Number(array[i].skills.length)) {
+        console.log(`Most Skilled worker is ${array[i].name} and has ${array[i].skills.length} skills`);
+        skillsNumber = array[i];
+      }
     }
 }
 
 
-
 // 3. Нэвтэрсэн хэрэглэгчдийг тоолж, дараах объектоос 50-аас дээш оноо авсан хэрэглэгчдийг тоол.
+console.log('\n3. Нэвтэрсэн хэрэглэгчдийг тоолж, дараах объектоос 50-аас дээш оноо авсан хэрэглэгчдийг тоол.');
+console.log('нийт нэвтэрсэн хэрэглэгчид = ' + users.length);
+findHighGrade(users);
+
+function findHighGrade(array) {
+  let highPointUsers = []
+  let counter = 0;
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].points >= 50) {
+      counter++
+    }
+  }
+  console.log("50-с дээш оноотой хэрэглэгч = " + counter);
+}
+
+
 // 4. Хэрэглэгчийн объектоос MERN ( MongoDB, Express, React, Node ) стек хөгжүүлэгч хүмүүсийг олоорой.
+console.log('\n4. Хэрэглэгчийн объектоос MERN ( MongoDB, Express, React, Node ) стек хөгжүүлэгч хүмүүсийг олоорой.');
+findMERN(users);
+
+function findMERN(array) {
+  let userMERN = []
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].skills.includes('MongoDB') && array[i].skills.includes('Express') && array[i].skills.includes('React') && array[i].skills.includes('Node')) {
+      console.log("MERN developer: " + array[i].name);
+    }
+  }
+}
+
+
 // 5. Хамгийн их настай 3 хүмүүсийн мэдээллийг хэвлэх
+console.log('\n5. Хамгийн хөгшин 3 хүмүүсийн мэдээллийг хэвлэх');
+findOlder(users);
+
+function findOlder(array) {
+  let userOlder = []
+  array.sort((firstItem, secondItem) => secondItem.age - firstItem.age);
+  for(let i = 0; i < 3; i++) {
+    console.log(JSON.stringify(array[i], null, 2));
+  }
+}
