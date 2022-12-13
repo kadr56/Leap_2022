254deed014a88dc8ee08f705894ad68b7039ffd3
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-13 16:02:42 +0800

    update

--- z-Assignments/js-method/method-7-car/main.js
+++ z-Assignments/js-method/method-7-car/main.js
@@ -1,8 +1,46 @@
 /*
 
-array - аас тодорхой элементийг устгах функц бичнэ үү
-example: console.log(removeArrayElement([2, 1, 6, 5, 8], 6))
-[2, 1, 5, 8]
+carBrands = [
+["Aston Martin Lagonda Ltd", "UK", 2016],
+["Audi", "Germany", 2016],
+["BMW", "Germany", 2016],
+["Chevrolet", "USA", 2016],
+["Dodge", "USA", 2016],
+["Ferrari", "Italy", 2016],
+["Honda", "Japan", 2016],
+["Jaguar", "UK", 2016],
+["Lamborghini", "Italy", 2016],
+];
+
+
+1. машины брэндийн эхний үсгээр хайх
+input: a
+output:   дараах байдлаар хэвлэнэ.
+
+brand: Aston Martin Lagonda Ltd
+country: UK
+year: 2016
+
+
+brand: Audi
+country: Germany
+year: 2016
+
+input: s
+
+output: олдсонгүй
+
+2. машины үйлвэрлэсэн улсаар хайх
+
+input: USA
+output: дараах байдлаар хэвлэнэ.
+
+
+Chevrolet, USA, 2016
+Dodge, USA,  2016
+
+3. нийт хэдэн ширхэг мөр дата байгаа, хэдэн улсын brand байгааг харуулах
+output:  Нийт 9 мөр дата, UK-2, Germany-2,  USA-2, Italy-2, Japan-1
 
 */
 
