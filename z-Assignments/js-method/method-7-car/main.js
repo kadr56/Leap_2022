e106f9191b3de60decbb63aaf339cd5bdeeb1dd4
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-13 21:28:56 +0800

    method-7-car exercise

--- z-Assignments/js-method/method-7-car/main.js
+++ z-Assignments/js-method/method-7-car/main.js
@@ -41,26 +41,48 @@ Dodge, USA,  2016
 
 3. нийт хэдэн ширхэг мөр дата байгаа, хэдэн улсын brand байгааг харуулах
 output:  Нийт 9 мөр дата, UK-2, Germany-2,  USA-2, Italy-2, Japan-1
-
 */
 
+carBrands = [
+   ["Aston Martin Lagonda Ltd", "UK", 2016],
+   ["Audi", "Germany", 2016],
+   ["BMW", "Germany", 2016],
+   ["Chevrolet", "USA", 2016],
+   ["Dodge", "USA", 2016],
+   ["Ferrari", "Italy", 2016],
+   ["Honda", "Japan", 2016],
+   ["Jaguar", "UK", 2016],
+   ["Lamborghini", "Italy", 2016],
+];
 
-let myArray = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log("My Array = " + myArray);   
+let input = prompt("Автомашины брэндийн эхний үсэг оруул").toLowerCase();
 
-let del = prompt(`My Array = ${myArray} \nInsert element from Array`);
-
-removeArrayElement(myArray, del);
-
-function removeArrayElement(arr, x) {
-   let deleteIndex = arr.indexOf(x);
-   if(deleteIndex > -1){   // only delete this value if it exists in the array
-      console.log("DELETED ITEM = " + arr.splice(deleteIndex, 1));
+for(let i = 0; i < carBrands.length; i++) {
+   console.log(carBrands[i][0].charAt(0).toLowerCase());
+   if(carBrands[i][0].charAt(0).toLowerCase() == input){
+      console.log("found");
+      
+      for(let j = 0; j < carBrands[i].length; j++) {
+         console.log(carBrands[i].join("\n"));
+      }
    } else {
-      console.log(x + " element is not found");
-   } 
-
-   console.log("My array after delete = " + myArray);   
+      console.log("not found")
+   }
 }
 
+/*
+1. машины брэндийн эхний үсгээр хайх
+input: a
+
+brand: Aston Martin Lagonda Ltd
+country: UK
+year: 2016
+
+brand: Audi
+country: Germany
+year: 2016
+
+input: s
+output: олдсонгүй
+*/
 
