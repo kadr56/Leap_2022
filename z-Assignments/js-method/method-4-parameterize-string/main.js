e37f0a6770433b558bdbd39b9437ff9c56b7f425
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-13 09:14:59 +0800

    update

--- z-Assignments/js-method/method-4-parameterize-string/main.js
+++ z-Assignments/js-method/method-4-parameterize-string/main.js
@@ -9,11 +9,14 @@ let str = "This is where i begin"
 parameterizeStr(str)
 
 function parameterizeStr(str) {
-    console.log(str.reverse());
-    
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === " ") {
+            str[i] === "-";
+        }
+    }
     // var splitString = str.split(""); 
-    // var reverseArray = splitString.reverse(); 
+    // var reverseArray = splitString.reverse(); ,
     // var joinArray = reverseArray.join(""); 
-    
-    // console.log(joinArray);
 }
+
+    console.log(joinArray);
