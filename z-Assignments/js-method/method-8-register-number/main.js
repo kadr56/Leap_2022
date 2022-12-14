cb4abb0389462fc18f344bb95923a6b5a1e43193
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-14 20:42:49 +0800

    register exercise

--- z-Assignments/js-method/method-8-register-number/main.js
+++ z-Assignments/js-method/method-8-register-number/main.js
@@ -30,68 +30,31 @@
 
 */
 
-carBrands = [
-   ["Aston Martin Lagonda Ltd", "UK", 2016],
-   ["Audi", "Germany", 2016],
-   ["BMW", "Germany", 2016],
-   ["Chevrolet", "USA", 2016],
-   ["Dodge", "USA", 2016],
-   ["Ferrari", "Italy", 2016],
-   ["Honda", "Japan", 2016],
-   ["Jaguar", "UK", 2016],
-   ["Lamborghini", "Italy", 2016],
-];
+
+/* 
+Дурын String авахад тухайн string нь зөв РД мөн эсэхийг шалгадаг isCorrectReg(register) функц бич. 
+Хэрэв зөв бол тухайн РД аас хүйс авдаг getGender(register) функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ. / 
+РД аас төрсөн өдөр авдаг getBirthDay(register) функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ. /
+РД аас төрсөн аймгийг олдог getBirthRegion(register)  функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ. /
+Дээрх бүх утгыг хэвлэдэг getRegisterInfo(register) функц бичнэ үү. */
 
 
-// 1. машины брэндийн эхний үсгээр хайх
-console.log("1. машины брэндийн эхний үсгээр хайх");
-let input = prompt("Автомашины брэндийн эхний үсэг оруул").toLowerCase();
+let input = prompt("Регистрийн дугаараа оруулна уу");
+isCorrectReg(input);
 
-for(let i = 0; i < carBrands.length; i++) {
-   if(carBrands[i][0].charAt(0).toLowerCase() == input){
-      console.log("brand : " + carBrands[i][0]);
-      console.log("country : " + carBrands[i][1]);
-      console.log("year : " + carBrands[i][2] + "\n\n");
-   } else {
-      console.log("олдсонгүй")
+
+function isCorrectReg(reg) {
+
+   console.log(isNaN(Number(reg.charAt(0))));
+   console.log(isNaN(Number(reg.charAt(1))));
+   console.log(reg.length == 10);
+   if((reg.length == 10 && isNaN(Number(reg.charAt(0))) && isNaN(Number(reg.charAt(1)))) ) {
+      
+      console.log("correct reg");
+   } else { 
+      console.log("Wrong REg");
    }
+    
+
+
 }
-
-
-
-// 2. машины үйлвэрлэсэн улсаар хайх
-console.log("\n2. машины үйлвэрлэсэн улсаар хайх");
-let country = prompt("Үйлдвэрлэсэн улсыг оруулна уу").toLowerCase();
-let found = false;
-let printTxt = "";
-
-for(let i = 0; i < carBrands.length; i++) {
-   if(carBrands[i][1].toLowerCase() === country){
-      printTxt += carBrands[i].join(", ") + "\n";
-      found = true;
-   } 
-}
-found ? console.log(printTxt) : console.log("олдсонгүй");
-
-
-// 3. нийт хэдэн ширхэг мөр дата байгаа, хэдэн улсын brand байгааг харуулах
-// output:  Нийт 9 мөр дата, UK-2, Germany-2,  USA-2, Italy-2, Japan-1
-console.log("\n3. нийт хэдэн ширхэг мөр дата байгаа, хэдэн улсын brand байгааг харуулах");
-let row = carBrands.length;
-console.log("Нийт мөр = " + row);
-let countries = [];
-for(let i = 0; i < carBrands.length; i++) {
-   countries.push(carBrands[i][1]);
-}
-
-countUniqueCountries(countries);
-
-function countUniqueCountries(arr) {
-   let count = [];
-   for(let i = 0; i < arr.length; i++) {
-      count[arr[i]] = 1 + (count[arr[i]] || 0);
-   }
-   console.log(count);
-}
-
-
