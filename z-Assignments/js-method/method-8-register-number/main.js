589df7d0af132c48c1b816169e643a376a5af0aa
Morning <morning@MStarss-iMac.local>
2022-12-19 08:54:44 +0800

    Register no

--- z-Assignments/js-method/method-8-register-number/main.js
+++ z-Assignments/js-method/method-8-register-number/main.js
@@ -41,13 +41,22 @@
 
 let input = prompt("Регистрийн дугаараа оруулна уу").toUpperCase();
 console.log("РД: " + input);
-isCorrectReg(input);
+
+
+
+getRegisterInfo(input);
+
+function getRegisterInfo(reg) {
+   isCorrectReg(reg);
+}
+
+
 
 
 // 1. Дурын String авахад тухайн string нь зөв РД мөн эсэхийг шалгадаг isCorrectReg(register) функц бич. 
 function isCorrectReg(reg) {
    if((reg.length == 10 && isNaN(Number(reg.charAt(0))) && isNaN(Number(reg.charAt(1)))) ) {  
-      // console.log("correct reg");
+      console.log("correct reg");
       getGender(reg);
       getBirthDay(reg);
       getBirthRegion(reg);
@@ -86,77 +95,73 @@ function getBirthRegion(reg) {
          console.log("Аймаг : Баян-Өлгий")
          break;
       case 'В':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Баянхонгор")
          break;
       case 'Г': 
-         console.log("Аймаг : Архангай");
+         console.log("Аймаг : Булган");
          break;
       case 'Д':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Говь-Алтай")
          break;
       case 'Е':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Дорноговь")
          break;
       case 'Ж': 
-         console.log("Аймаг : Архангай");
+         console.log("Аймаг : Дорнод");
          break;
       case 'З':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Дундговь")
          break;
       case 'И':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Завхан")
          break;
       case 'Й': 
-         console.log("Аймаг : Архангай");
+         console.log("Аймаг : Өвөрхангай");
          break;
       case 'К':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Өмнөговь")
          break;
       case 'Л':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Сүхбаатар")
          break;
       case 'М': 
-         console.log("Аймаг : Архангай");
+         console.log("Аймаг : Сэлэнгэ");
          break;
       case 'Н':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Төв")
          break;
       case 'О':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Увс")
          break;
       case 'П': 
-         console.log("Аймаг : Архангай");
+         console.log("Аймаг : Ховд");
          break;
       case 'Р':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Хөвсгөл")
          break;
       case 'С':
-         console.log("Аймаг : Баян-Өлгий")
+         console.log("Аймаг : Хэнтий")
+         break;
+      case 'Т':
+         console.log("Аймаг : Дархан-Уул")
+         break;
+      case 'Ф': 
+         console.log("Аймаг : Орхон");
+         break;
+      case 'Х':
+         console.log("Аймаг : Говьсүмбэр")
+         break;
+      case 'У':
+         console.log("Аймаг : Улаанбаатар")
+         break;
+      case 'Ц':
+         console.log("Аймаг : Улаанбаатар")
+         break;
+      case 'Ч':
+         console.log("Аймаг : Улаанбаатар")
+         break;
+      default:
+         console.log("Буруу РД оруулсан байна. Аймаг олдсонгүй.")
          break;
    }   
 }
-
-А – Архангай
-      Б – Баян-Өлгий
-      В – Баянхонгор
-      Г – Булган
-      Д – Говь-Алтай
-      Е – Дорноговь
-      Ж – Дорнод
-      З – Дундговь
-      И – Завхан
-      Й – Өвөрхангай
-      К – Өмнөговь
-      Л – Сүхбаатар
-      М – Сэлэнгэ
-      Н – Төв
-      О – Увс
-      П – Ховд
-      Р – Хөвсгөл
-      С – Хэнтий
-      Т – Дархан-Уул
-      Ф – Орхон
-      Х – Говьсүмбэр
-      У,Ц – Улаанбаатар
-
-// 5. Дээрх бүх утгыг хэвлэдэг getRegisterInfo(register) функц бичнэ үү. 
