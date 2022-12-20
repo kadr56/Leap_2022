cc3746826c031711c94e73688d1deb0ee5575bbd
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-20 21:39:42 +0800

    guess my number exercise

--- z-Assignments/js-DOM/object-6-guess-my-number/script.js
+++ z-Assignments/js-DOM/object-6-guess-my-number/script.js
@@ -9,28 +9,69 @@
 */
 
 let input = document.querySelector('.guess');
-let checkBtn = document.querySelector('.btn-check')
+let checkBtn = document.querySelector('.check')
+let resetBtn = document.querySelector('.again')
 let message = document.querySelector('.message')
+let scoreBoard = document.querySelector('.score');
 let randomNumber = Math.floor((Math.random() * 20) + 1 )
 let score = 20;
 console.log('random number = ' + randomNumber);
 
 checkBtn.addEventListener('click', checkInput)
+resetBtn.addEventListener('click', reset)
 
 function checkInput() {
-    console.log(input.value);
+    // console.log(input.value);
     console.log(checkBtn);
     if(!Number.isInteger(Number(input.value))) {
-        console.log("it is not integer");
+        // console.log("it is not integer");
         message.textContent = 'Тоо оруулна уу.'
-        if(input.value === randomNumber) {
-            message.textContent = 'Та хожлоо.'
-        } else if (input.value > randomNumber) {
-            message.textContent = 'Их байна.'
-        }
     } else {
-        console.log("it is integer")
+        if(score > 0) {
+            console.log("it is integer")
+        if(Number(input.value) == randomNumber) {
+            // console.log(input.value + ' == ' + randomNumber + "??????")
+            message.textContent = 'Зөв таалаа.'
+            scoreBoard.textContent = score;
+            document.body.style.backgroundColor = "#60b347";
+        } 
+        if (Number(input.value) > randomNumber) {
+            score--;
+            scoreBoard.textContent = score;
+            message.textContent = 'Их байна.'
+            console.log('Их байна');
+        } 
+        if (Number(input.value) < randomNumber) {
+            score--;
+            scoreBoard.textContent = score;
+            message.textContent = 'Бага байна.'
+            console.log('Бага байна');
+        }
+        } else {
+            scoreBoard.textContent = score;
+            message.textContent = 'Та хожигдлоо.'
+            document.body.style.backgroundColor = "red";
+            input.disabled = true;
+            checkBtn.disabled = true;
+                // input.parentElement.children[0].disabled = true;
+
+
+        }
     }
+}
+
+
+function reset() {
+    document.body.style.backgroundColor = "black";
+    score = 20;
+    scoreBoard.textContent = score;
+    // input.parentElement.children[0].disabled = true;
+    
+    input.value = '';
+    randomNumber = Math.floor((Math.random() * 20) + 1 )
+    console.log('Random = ' + randomNumber);
+    input.disabled = false;
+    checkBtn.disabled = false;
 }
 
 
